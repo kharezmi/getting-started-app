5786230700f0c3df716d21b9f6e7682d3af44d72
Abdullaziz <abdullaziz@ug.bilkent.edu.tr>
2025-04-25 02:11:12 +0300

    NEw commit

--- src/static/js/app.js
+++ src/static/js/app.js
@@ -53,7 +53,7 @@ function TodoListCard() {
         <React.Fragment>
             <AddItemForm onNewItem={onNewItem} />
             {items.length === 0 && (
-                <p className="text-center">You have no to do tasks now.!</p>
+                <p className="text-center">You have to do tasks now.!</p>
             )}
             {items.map(item => (
                 <ItemDisplay
